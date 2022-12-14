64ccff2558e319b8cfd57aee328235b2426fab04
Simon <simonaucoin11@gmail.com>
2022-12-13 20:00:27 -0500

    fix

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,16 @@
-package main
+package web
 
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/sidramon/AxoloGo/internal/handlers"
 )
 
 const port = ":3000"
 
 func main() {
-	http.HandleFunc("/", handlers.)
+	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/contact", handlers.Contact)
 
 	fmt.Println("(http://localhost:3000) - Server started on port", port)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sidramon/AxoloGo
+
+go 1.19
